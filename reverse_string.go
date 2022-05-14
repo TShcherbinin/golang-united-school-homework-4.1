9e94d1b926ad5d21f2366f3bf1fe808813ae80b3
Timofey <sherbinin5@gmail.com>
2022-05-14 20:49:16 +0300

    Task41. Reserse strings

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,14 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	tmp := []rune(input)
+	strLen := len(tmp)
+	// output = input
+
+	for i, l := 0, strLen/2; i < l; i++ {
+		tmp[strLen-i-1], tmp[i] = tmp[i], tmp[strLen-i-1]
+		//output[i] = input[strLen-i-1]
+	}
+
+	return string(tmp)
 }
